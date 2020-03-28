9d2b6c8e61a4b0e2d825ab1ba5f518bad83f64b3
nthxbi <nthabielle@live.com>
2020-03-28 21:04:28 +0200

    fixed failing test class

--- pet-clinic-web/src/test/java/guru/framework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/framework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -3,7 +3,7 @@ package guru.framework.sfgpetclinic;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest  
 class SfgPetClinicApplicationTests {
 
     @Test
