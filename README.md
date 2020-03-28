e5cb69e165f49b1ba0122c509b41035a95d026ec
nthxbi <nthabielle@live.com>
2020-03-28 21:03:44 +0200

    fixed failing test class

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # sfg-pet-clinic
-SFT Pet Clinic
+SFT guru.framework.sfgpetclinic.model.Pet Clinic
--- pet-clinic-web/src/test/java/guru/framework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/framework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,4 +1,4 @@
-package sfgpetclinic;
+package guru.framework.sfgpetclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
